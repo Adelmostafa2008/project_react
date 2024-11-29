c5eb19cfe784bcd222d75c596fa22770990096d0
Adelmostafa2008 <adel.0523035@gmail.com>
2024-11-29 21:52:11 +0200

    first

---
+++ first_project/src/card.jsx
@@ -0,0 +1,15 @@
+import pic from './assets/rizz.png'
+
+function Card(){
+    return(
+        <>
+        <div className="card">
+        <img alt="rizz asf" src={pic} className="pic"></img>
+        <h2>My First Card</h2>
+        <p>look at me gat daim i look smooth asf bro am gonna make millions out of this shit bro</p>
+        </div>
+        </>
+    );
+}
+
+export default Card
